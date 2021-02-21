6be842460a22bbe7fde8113ee1448975fe779002
FikryAS <azizfikry15@gmail.com>
2021-02-21 12:14:55 +0700

    checking data value empty and restore data to value parent register content excapt password

--- proses_register.php
+++ proses_register.php
@@ -9,11 +9,19 @@ $nama_lengkap = $_POST['nama_lengkap'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $alamat = $_POST['alamat'];
-$password = md5($_POST['password']);
+$password = $_POST['password'];
 $re_password = $_POST['re_password'];
 
-mysqli_query(
-  $koneksi,
-  "INSERT INTO user (level, nama, email, alamat, phone, password, status)
-                        VALUES ('$level', '$nama_lengkap', '$email', '$alamat', $phone, '$password', '$status')"
-);
+unset($_POST['password']);
+unset($_POST['re_password']);
+$dataForm = http_build_query($_POST);
+
+if (empty($nama_lengkap) || empty($email) || empty($phone) || empty($alamat) || empty($password) || empty($re_password)) {
+  header("location:" . BASE_URL . "index.php?page=register&notif=require&$dataForm");
+} else {
+  mysqli_query(
+    $koneksi,
+    "INSERT INTO user (level, nama, email, alamat, phone, password, status)
+                          VALUES ('$level', '$nama_lengkap', '$email', '$alamat', $phone, '$password', '$status')"
+  );
+}
--- register.php
+++ register.php
@@ -2,24 +2,36 @@
 
   <form action="<?= BASE_URL . "proses_register.php" ?>" method="POST">
 
+    <?php
+    $notif = isset($_GET['notif']) ? $_GET['notif'] : false;
+    $nama_lengkap = isset($_GET['nama_lengkap']) ? $_GET['nama_lengkap'] : false;
+    $email = isset($_GET['email']) ? $_GET['email'] : false;
+    $phone = isset($_GET['phone']) ? $_GET['phone'] : false;
+    $alamat = isset($_GET['alamat']) ? $_GET['alamat'] : false;
+
+    if ($notif == "require") {
+      echo "<div class='notif'>Maaf, kamu harus melengkapi form dibawah ini</div>";
+    }
+    ?>
+
     <div class="element-form">
       <label>Nama Lengkap</label>
-      <span><input type="text" name="nama_lengkap" /></span>
+      <span><input type="text" name="nama_lengkap" value="<?= $nama_lengkap ?>" /></span>
     </div>
 
     <div class="element-form">
       <label>Email</label>
-      <span><input type="email" name="email" /></span>
+      <span><input type="email" name="email" value="<?= $email ?>" /></span>
     </div>
 
     <div class="element-form">
       <label>Nomor Telepon / Phone</label>
-      <span><input type="number" name="phone" /></span>
+      <span><input type="number" name="phone" value="<?= $phone ?>" /></span>
     </div>
 
     <div class="element-form">
       <label>Alamat</label>
-      <span><textarea name="alamat"></textarea></span>
+      <span><textarea name="alamat"><?= $alamat ?></textarea></span>
     </div>
 
     <div class="element-form">
