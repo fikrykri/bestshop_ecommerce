5d782b6a5dfe6a7b7cb108409cf1a42c0db571d4
FikryAS <azizfikry15@gmail.com>
2021-02-22 12:21:30 +0700

    change the table view using css

--- module/kategori/list.php
+++ module/kategori/list.php
@@ -2,41 +2,40 @@
 
   <a href="<?= BASE_URL . "index.php?page=my_profile&module=kategori&action=form" ?>" class="tombol-action">+ Tambah Kategori</a>
 
-  <?php
+</div>
+<?php
 
-  $queryKategory = mysqli_query($koneksi, "SELECT * FROM kategori");
+$queryKategory = mysqli_query($koneksi, "SELECT * FROM kategori");
 
-  if (mysqli_num_rows($queryKategory) == 0) {
-    echo "<h3>Saat ini beluma da nama kategori di dalam tabel kategori</h3>";
-  } else {
-    echo "<table class='table-list'>";
+if (mysqli_num_rows($queryKategory) == 0) {
+  echo "<h3>Saat ini beluma da nama kategori di dalam tabel kategori</h3>";
+} else {
+  echo "<table class='table-list'>";
 
-    echo "<tr>
-            <th>No</th>
-            <th>Kategori</th>
-            <th>Status</th>
-            <th>Action</th>
+  echo "<tr class='baris-title'>
+            <th class='kolom-nomor'>No</th>
+            <th class='kiri'>Kategori</th>
+            <th class='tengah'>Status</th>
+            <th class='tengah'>Action</th>
           </tr>";
 
-    $no = 1;
-    while ($row = mysqli_fetch_assoc($queryKategory)) {
+  $no = 1;
+  while ($row = mysqli_fetch_assoc($queryKategory)) {
 
-      echo "<tr>
-              <td>$no</td>
-              <td>$row[kategori]</td>
-              <td>$row[status]</td>
-              <td>
+    echo "<tr class='baris-data'>
+              <td class='kolom-nomor'>$no</td>
+              <td class='kiri'>$row[kategori]</td>
+              <td class='tengah'>$row[status]</td>
+              <td class='tengah'>
                 <a href='" . BASE_URL . "index.php?page=my_profile&module=kategori&action=form&kategori_id=$row[kategori_id]' class='tombol-action'>Edit</a>
 
               </td>
             </tr>";
 
-      $no++;
-    }
-
-    echo "</table>";
+    $no++;
   }
 
-  ?>
+  echo "</table>";
+}
 
-</div>
+?>
