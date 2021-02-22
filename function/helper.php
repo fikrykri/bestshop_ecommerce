bcfcc69e75eb67b2be29cf58cdf3cdf4b66d6cee
FikryAS <azizfikry15@gmail.com>
2021-02-22 13:44:44 +0700

    add rupiah function in file helper

--- function/helper.php
+++ function/helper.php
@@ -1,3 +1,9 @@
 <?php
 // code dibawah merupakan konstanta BASE_URL yang isi/ valuenya yang disebelah kanannya
 define("BASE_URL", "http://localhost/bestshop_ecommerce/");
+
+function rupiah($nilai = 0)
+{
+  $string = "Rp." . number_format($nilai);
+  return $string;
+}
