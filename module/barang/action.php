c5a60603436a7b90432196fd084638bdc4d3da33
FikryAS <azizfikry15@gmail.com>
2021-02-22 15:14:09 +0700

    add the update action to the item module

--- module/barang/action.php
+++ module/barang/action.php
@@ -6,24 +6,32 @@ include_once("../../function/helper.php");
 $nama_barang = $_POST['nama_barang'];
 $kategori_id = $_POST['kategori_id'];
 $spesifikasi = $_POST['spesifikasi'];
-$harga = $_POST['harga'];
-$stok = $_POST['stok'];
 $status = $_POST['status'];
 $button = $_POST['button'];
+$harga = $_POST['harga'];
+$stok = $_POST['stok'];
+$update_gambar = "";
 
-$nama_file = $_FILES['file']['name'];
-move_uploaded_file($_FILES['file']['tmp_name'], "../../images/barang/" . $nama_file);
+if (!empty($_FILES["file"]["name"])) {
+  $nama_file = $_FILES["file"]["name"];
+  move_uploaded_file($_FILES["file"]["tmp_name"], "../../images/barang/" . $nama_file);
+
+  $update_gambar = ", gambar='$nama_file'";
+}
 
 if ($button == "Add") {
   mysqli_query($koneksi, "INSERT INTO barang (nama_barang, kategori_id, spesifikasi, gambar, harga, stok, status ) 
-                          VALUES ('$nama_barang', '$kategori_id', '$spesifikasi', '$nama_file', $harga, $stok, '$status')");
+                          VALUES ('$nama_barang', '$kategori_id', '$spesifikasi', '$nama_file', '$harga', '$stok', '$status')");
+} else if ($button == "Update") {
+  $barang_id = $_GET['barang_id'];
+
+  mysqli_query($koneksi, "UPDATE barang SET kategori_id='$kategori_id',
+                                            nama_barang='$nama_barang', 
+                                            spesifikasi='$spesifikasi',
+                                            harga='$harga',
+                                            stok='$stok',
+                                            status='$status'
+                                            $update_gambar WHERE barang_id='$barang_id'");
 }
-// else if ($button == "Update") {
-//   $kategori_id = $_GET['kategori_id'];
-
-//   mysqli_query($koneksi, "UPDATE kategori SET kategori = '$kategori', 
-//                                               status ='$status' WHERE kategori_id = '$kategori_id'");
-// }
-
 
 header("location:" . BASE_URL . "index.php?page=my_profile&module=barang&action=list");
