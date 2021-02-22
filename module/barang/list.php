0e6c3db7dbf64c389bb8b0d28dfe44d7329a1d13
FikryAS <azizfikry15@gmail.com>
2021-02-22 13:28:19 +0700

    create a join query between module item and module category

--- module/barang/list.php
+++ module/barang/list.php
@@ -5,7 +5,7 @@
 </div>
 <?php
 
-$query = mysqli_query($koneksi, "SELECT * FROM barang");
+$query = mysqli_query($koneksi, "SELECT barang.*, kategori.kategori FROM barang JOIN kategori ON barang.kategori_id = kategori.kategori_id ORDER BY nama_barang ASC");
 
 if (mysqli_num_rows($query) == 0) {
   echo "<h3>Saat ini beluma ada barang di dalam tabel barang</h3>";
@@ -15,6 +15,7 @@ if (mysqli_num_rows($query) == 0) {
   echo "<tr class='baris-title'>
             <th class='kolom-nomor'>No</th>
             <th class='kiri'>Barang</th>
+            <th class='kiri'>Kategori</th>
             <th class='kiri'>Harga</th>
             <th class='tengah'>Status</th>
             <th class='tengah'>Action</th>
@@ -26,6 +27,7 @@ if (mysqli_num_rows($query) == 0) {
     echo "<tr class='baris-data'>
               <td class='kolom-nomor'>$no</td>
               <td class='kiri'>$row[nama_barang]</td>
+              <td class='kiri'>$row[kategori]</td>
               <td class='kiri'>$row[harga]</td>
               <td class='tengah'>$row[status]</td>
               <td class='tengah'>
