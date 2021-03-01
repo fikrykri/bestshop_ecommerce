c82c8c153ba75ebf96f27e69bdbce1add72dcc0f
FikryAS <azizfikry15@gmail.com>
2021-03-01 14:16:37 +0700

    standart URL SEO friendly use 3 variable on barang_id, kategori name, and nama_barang

--- main.php
+++ main.php
@@ -24,13 +24,18 @@
     <ul>
       <?php
       if ($kategori_id) {
-        $kategori_id = "AND kategori_id=$kategori_id";
+        $kategori_id = "AND kategori.kategori_id=$kategori_id";
       }
-      $query = mysqli_query($koneksi, "SELECT * FROM barang WHERE status='on' $kategori_id ORDER BY rand() DESC LIMIT 9");
+      $query = mysqli_query($koneksi, "SELECT barang.*, kategori.kategori FROM barang JOIN kategori ON barang.kategori_id = kategori.kategori_id WHERE barang.status='on' $kategori_id ORDER BY rand() DESC LIMIT 9");
 
 
       $no = 1;
       while ($row = mysqli_fetch_assoc($query)) :
+
+        $kategori = strtolower($row['kategori']);
+        $barang = strtolower($row['nama_barang']);
+        $barang = str_replace(" ", "-", $barang);
+
         $style = false;
         if ($no == 3) {
           $style = "style='margin-right:0px;'";
@@ -39,11 +44,11 @@
       ?>
         <li <?= $style ?>>
           <p class="price"><?= rupiah($row['harga']) ?></p>
-          <a href="<?= BASE_URL . "index.php?page=detail&barang_id=$row[barang_id]" ?>">
+          <a href="<?= BASE_URL . "$row[barang_id]/$kategori/$barang.html" ?>">
             <img src="<?= BASE_URL . "/images/barang/$row[gambar]" ?>" />
           </a>
           <div class="keterangan-gambar">
-            <p><a href="<?= BASE_URL . "index.php?page=detail&barang_id=$row[barang_id]" ?>"><?= $row['nama_barang'] ?></a></p>
+            <p><a href="<?= BASE_URL . "$row[barang_id]/$kategori/$barang.html" ?>"><?= $row['nama_barang'] ?></a></p>
             <span>Stok : <?= $row['stok'] ?></span>
           </div>
           <div class="btn-add-cart">
